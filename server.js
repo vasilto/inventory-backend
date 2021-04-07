ac724e5d26dd803dde86339a5500c131f2cf6fe9
vasilto <vasil.a.todorov@gmail.com>
2021-04-07 20:43:26 +0300

    adding cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -6,6 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGO_URI;
